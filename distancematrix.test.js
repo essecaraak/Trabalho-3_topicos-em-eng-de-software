654212250366c04ca8a7dcefaf2c0a0e48bdc010
essecaraak <matheusoliveiradesouza190503@gmail.com>
2024-07-18 20:26:20 -0300

    ctmatrix

--- distancematrix.test.js
+++ distancematrix.test.js
@@ -1,9 +1,10 @@
-/*const { getDistanceMatrix } = require('./distanceMatrix');
+const { getDistanceMatrix } = require('./distanceMatrix');
 require('dotenv').config();
 
 describe('Distance Matrix API Tests', () => {
-  const validKey = process.env.GOOGLE_API_KEY; // Insira sua chave de API válida aqui
-
+  const validKey = process.env.apiKey; // Insira sua chave de API válida aqui
+  const invalidKey = "invalid";
+  const intKey = parseInt(validKey);
   const testCases = [
     {
       id: 'CT01',
@@ -12,13 +13,10 @@ describe('Distance Matrix API Tests', () => {
       apiKey: validKey,
       expected: [
         [
-          { distance: { text: "6.6 km" }, duration: { text: "15 mins" } },
-          { distance: { text: "2,160 km" }, duration: { text: "1 day 6 hours" } }
+          { distance: { min: "6.6 km", max:"7km"}, duration: { min: "15 minutos",max:"1horas"} },
+          { distance: { min: "2,160 km", max:"70000km"}, duration: { min: "1 dia 6 horas",max:"2dias" } }
         ],
-        [
-          { distance: { text: "1,824 km" }, duration: { text: "1 day 3 hours" } },
-          { distance: { text: "435 km" }, duration: { text: "5 hours 40 mins" } }
-        ]
+        
       ]
     },
     {
@@ -39,15 +37,27 @@ describe('Distance Matrix API Tests', () => {
   testCases.forEach(({ id, origins, destinations, apiKey, expected }) => {
     test(`${id} - Testing Distance Matrix API`, async () => {
       try {
-        const data = await getDistanceMatrix(origins, destinations, apiKey);
-
+        let apiKeyToUse = validKey; // Default to validKey
+        if (apiKey === "invalidkey") {
+          apiKeyToUse = invalidKey;
+        } else if (apiKey === "integerkey") {
+          apiKeyToUse = intKey;
+        }else if (apiKey === null) {
+          apiKeyToUse = null;
+        }else if (apiKey === "doublekey") {
+          apiKeyToUse = [validKey,validKey];
+        }
+        const data = await getDistanceMatrix(origins, destinations, apiKeyToUse);
+        if (expected.status) {
+          await expect(data.status).toBe(expected.status);
+        } else {
         data.rows.forEach((row, rowIndex) => {
           row.elements.forEach((element, colIndex) => {
             expect(element.distance.text).toBe(expected[rowIndex][colIndex].distance.text);
             expect(element.duration.text).toBe(expected[rowIndex][colIndex].duration.text);
           });
         });
-
+      }
       } catch (error) {
         if (expected.error) {
           expect(error).toBeDefined();
@@ -57,4 +67,4 @@ describe('Distance Matrix API Tests', () => {
       }
     });
   });
-});*/
+});
